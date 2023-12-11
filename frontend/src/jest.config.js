ba08f99a562348aca1c129e2c660171ff3ca5a0c
dev <dev@localhost.localdomain>
2023-12-11 21:24:09 +0530

    testing files

--- frontend/src/jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // ... other configuration options
-    moduleNameMapper: {
-      '^@/(.*)$': '<rootDir>/src/$1',
-    },
-    transform: {
-      '^.+\\.(js|jsx|ts|tsx)$': ['babel-jest', { presets: ['@babel/preset-env', '@babel/preset-react'] }],
-    },
-  };
